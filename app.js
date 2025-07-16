5519713a5989deedc1fb90b195096f54b768c6b7
José vitor <jvitinhosa@gmail.com>
2025-07-16 11:36:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-/*aula
+/*Aula 01:
 
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Jogo do número secreto";
 
-let paragrafo = document.querySelector("p");
-paragrafo.innerHTML = "Escolha um número 1 e 10"; 
+let numeroSecreto = gerarNumeroSecreto();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = documet.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 100");
 
 function verificarChute() {
-    console.log("O botão foi clicado!");
+    let chute =  document.querySelector("input").value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumero() {
+    return  parseInt(Math.random() * 10 + 1);
 }
 */
 
+
+
 /*
-//desafio01
+//Desafio 01:
 //1
 let titulo = document.querySelector("h1");
 titulo.innerHTML = "Hora do desafio";
@@ -23,23 +35,46 @@ function verificarChute() {
     console.log("clicado");    
     alert(clicado);
 }
+    
 //3
 function verificarChute() {
     alert("Amo js");
 }
+
 //4 
 function verificarChute() {
     let cidade = prompt("Digite um nome de uma cidade: ");
     alert(`Estive em ${cidade} lembrei de você`)
-}
-*/
-
-
+} 
 
 //5
 function verificarChute() {
     let numeroInteiro = parseInt(prompt("Insira um número: "));
-    let numeroInteiro2  = parseInt(prompt("Insira outro número: "));
+    let numeroInteiro2 = parseInt(prompt("Insira outro número: "));
     let resultado = numeroInteiro + numeroInteiro2; 
     alert("Resultado é " + resultado);
+}
+*/
+
+
+/*Aula 02:
+Desafio 02:
+//1
+function verificarChute() {
+    console.log("ola, mundo");
+}
+
+//2
+function verificarChute() {
+    let nome = prompt("Digite um nome de uma cidade: ");
+    console.log(`Olá ${nome} lembrei de você`);
+}
+*/
+
+
+//3
+function verificarChute() { 
+    let n =  document.querySelector("input").value;
+    let resultado = n * 2;
+    console.log(resultado);
 }
